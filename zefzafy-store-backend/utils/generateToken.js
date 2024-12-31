f6d0361f50b3cdb7891d6551c92fbe0247a98d0d
mohamed-Elzefzafy <your-email@example.com>
2024-12-31 21:56:05 +0200

    deploy 2 31-12

--- zefzafy-store-backend/utils/generateToken.js
+++ zefzafy-store-backend/utils/generateToken.js
@@ -6,10 +6,18 @@ dotenv.config({path : "../config.env"});
 export const generateToken = (res , userId) => {
   const token = jwt.sign({userId : userId} , process.env.JWT_SECRET , {expiresIn : "30d"});
 
+  // res.cookie("jwt" , token, {
+  //   httpOnly : true,
+  //   secure: true,
+  //   sameSite: 'strict',
+  //   maxAge : 30 * 24 * 60 * 60 * 1000,
+  //   secure: true,
+  //   sameSite: 'none',
+  // }) 
   res.cookie("jwt" , token, {
     httpOnly : true,
-    secure: true,
-    sameSite: 'strict',
+    secure: process.env.NODE_ENV !== "development",
+    // sameSite: 'strict',
     maxAge : 30 * 24 * 60 * 60 * 1000,
     // secure: true,
     sameSite: 'none',
